6f316438ef4ecb60b96d5f64ecab7bd6875d72c5
jackE-code <jackbaawer.007@gmail.com>
2024-12-09 11:42:39 +0300

    First Commit

---
+++ app/src/main/java/com/insurance/app/ChefRegistration.java
@@ -0,0 +1,14 @@
+package com.insurance.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChefRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_registration);
+    }
+}
